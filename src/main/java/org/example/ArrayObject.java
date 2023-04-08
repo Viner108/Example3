e2d8ae849975f5897ddf663861b160fe59fe9edd
Viner108 <andrukovichl@mail.ru>
2023-04-08 18:31:27 +0300

    getCount and getAdress +comments commit

--- src/main/java/org/example/ArrayObject.java
+++ src/main/java/org/example/ArrayObject.java
@@ -6,17 +6,19 @@ import java.util.stream.IntStream;
 
 public class ArrayObject {
     public static void main(String[] args) {
-        int[] array = intit(20);//создает массив заполненный -1
+        int[] array = intit(40);//создает массив заполненный -1
         int[] object = new int[6]; // первый объект для размещенние в массив с нулевыми значениями
         int[] object2 = new int[6];// второй объект для размещенние в массив с нулевыми значениями
-        int[] object3 = new int[2];// третий объект для размещенние в массив с нулевыми значениями
-        int indexofobject = 1; // индекс объекта в котором нужно заменить значение
-        int value = 7;// значение для замены в объекте
-        int index = 2;// индекс значение в объкте который нужно заменить
+        int[] object3 = new int[13];// третий объект для размещенние в массив с нулевыми значениями
+        int indexofobject = 3; // индекс объекта в котором нужно заменить значение
+        int value = 131;// значение для замены в объекте
+        int index = 9;// индекс значение в объкте который нужно заменить
         objectCreate(array, object); // функция для размещение первого объкта в массиве
         objectCreate(array, object2);// функция для размещение второго объкта в массиве
         objectCreate(array, object3);// функция для размещение третьего объкта в массиве
         update(array, index, indexofobject, value); //функция для замены значения в определенной объекте
+        System.out.println(getCount(array));
+        System.out.println(getAdress(array,indexofobject));
     }
 
     public static int[] intit(int size) { // функция для создания и заполнения исходного массива
@@ -53,15 +55,35 @@ public class ArrayObject {
     public static boolean update(int[] array, int index, int indexofobject, int value) {//функиця для замены значения в
         //оперделенной объекте
         int j = 0;
-        for (int i = 0; i < array.length; i = i + array[i] + 1) {
+        for (int i = 0; array[i] != -1; i = i + array[i] + 1) {
             j++;
-            if (j == indexofobject && array[i + index + 1] != -1) {
+            if (j == indexofobject &&index<array[i]) {
                 array[i + index + 1] = value;
                 printLines(array);
                 return true;
             }
         }
         return false;
+    }
+
+    public static int getCount(int[] array) {//функция считает количество объектов в массиве
+        int j = 0;
+        int i = 0;
+        while (array[i] != -1) {
+            i = i + array[i] + 1;
+            j++;
+        }
+        return j;
+    }
+
+    public static int getAdress(int[] array, int indexofobject) {//функция пвзвращающая индекс начала определенного объекта
+        int j = 0;
+        for (int i = 0; array[i] != -1; i = i + array[i] + 1) {
+            j++;
+            if (j == indexofobject)
+                return i;
+        }
+        return -1;
     }
 
     private static void printLines(int[] lines) {//функция для выведения массива на консоль
